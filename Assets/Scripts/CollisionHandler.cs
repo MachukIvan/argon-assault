6ad4d405a35b5d9c18b8086de0f27ebb5ac83aa1
Ivan Machuk <machuk.ivan@yandex.by>
2023-03-15 22:58:03 +0100

    Added explosion vfx

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadLevelDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
 
     void OnTriggerEnter(Collider other)
     {
@@ -15,7 +16,10 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence()
     {
+        crashVFX.Play();
         gameObject.GetComponent<PlayerControls>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
         Invoke("ReloadLevel", loadLevelDelay);
     }
 
